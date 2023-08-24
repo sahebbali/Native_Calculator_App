519809a8b2c41abd1bf44c5cddd6dadaa41ef149
sahebbali <sahebbali253@gmail.comm>
2023-08-24 20:27:28 +0600

    add Intro Screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import IntroScreen from './src/Screens/IntroScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <IntroScreen/>
       <StatusBar style="auto" />
     </View>
   );
